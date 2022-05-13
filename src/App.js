c69eccea14078222a2933a62965653ac6c6908a3
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-05-13 12:37:18 +0500

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { DatePicker } from "antd";
+import { Home } from "./components/home/Home";
+
+function App() {
+  return (
+    <div>
+      <DatePicker />
+      hello world !
+      <Home />
+    </div>
+  );
+}
+export default App;
